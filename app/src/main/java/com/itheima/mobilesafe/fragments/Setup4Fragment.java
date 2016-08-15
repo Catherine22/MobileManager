3eb349d2d8615703fb4089de7c879d83771a73a6
Catherine <yacatherine19@gmail.com>
2016-08-15 15:31:49 +0800

    Added: hiding keyboard setupFragments fixed subtitle error on SettingItemView

---
+++ app/src/main/java/com/itheima/mobilesafe/fragments/Setup4Fragment.java
@@ -0,0 +1,60 @@
+package com.itheima.mobilesafe.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.itheima.mobilesafe.Constants;
+import com.itheima.mobilesafe.MainInterface;
+import com.itheima.mobilesafe.R;
+import com.itheima.mobilesafe.ui.SettingItemView;
+
+/**
+ * Created by Catherine on 2016/8/12.
+ * Soft-World Inc.
+ * catherine919@soft-world.com.tw
+ */
+public class Setup4Fragment extends Fragment {
+
+    private static final String TAG = "Setup3Fragment";
+    private MainInterface mainInterface;
+    private Button bt_next, bt_back;
+    private SettingItemView siv_sim;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setup3, container, false);
+        mainInterface = (MainInterface) getActivity();
+        bt_next = (Button) view.findViewById(R.id.bt_next);
+        bt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainInterface.callFragment(Constants.SETUP2_FRAG);
+            }
+        });
+        bt_back = (Button) view.findViewById(R.id.bt_back);
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainInterface.backToPreviousPage();
+            }
+        });
+        siv_sim = (SettingItemView) view.findViewById(R.id.siv_sim);
+        siv_sim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (siv_sim.isChecked()) {
+
+                } else {
+
+                }
+            }
+        });
+        return view;
+    }
+}
