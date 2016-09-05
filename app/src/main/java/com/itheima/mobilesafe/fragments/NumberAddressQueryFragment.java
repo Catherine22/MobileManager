34394231b05f98f709c964d768711ca0ee8d29d5
Catherine <yacatherine19@gmail.com>
2016-09-05 09:08:40 +0800

    fix conflicting

--- app/src/main/java/com/itheima/mobilesafe/fragments/NumberAddressQueryFragment.java
+++ app/src/main/java/com/itheima/mobilesafe/fragments/NumberAddressQueryFragment.java
@@ -17,22 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.itheima.mobilesafe.R;
-<<<<<<< HEAD
 import com.itheima.mobilesafe.utils.TelephoneUtils;
-=======
-import com.itheima.mobilesafe.db.dao.NumberAddressDao;
-import com.itheima.mobilesafe.utils.CLog;
-import com.itheima.mobilesafe.utils.Constants;
-import com.itheima.mobilesafe.utils.HttpTools;
-import com.itheima.mobilesafe.utils.Settings;
 import com.itheima.mobilesafe.utils.ViewKits;
-import com.itheima.mobilesafe.utils.XMLPullParserHandler;
-import com.itheima.mobilesafe.utils.objects.MobileQuery;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
->>>>>>> 9b01e5eaaa4cc8d3b8ecaf29118bb3c28ced29f9
 
 /**
  * Created by Catherine on 2016/8/25.
@@ -116,37 +102,4 @@ public class NumberAddressQueryFragment extends Fragment {
             Toast.makeText(getActivity(), "您还没输入电话号码!", Toast.LENGTH_SHORT).show();
     }
 
-<<<<<<< HEAD
-=======
-    private Handler myHandler = new Handler() {
-        @Override
-        public void handleMessage(final Message msg) {
-
-            if (msg.what == Constants.SENT_SUCCESSFULLY) {
-                Bundle bundle = msg.getData();
-                String message = bundle.getString("MSG");
-                CLog.d(TAG, message);
-                try {
-                    InputStream stream = new ByteArrayInputStream(message.getBytes("GBK"));
-                    XMLPullParserHandler xmlParser = new XMLPullParserHandler();
-                    MobileQuery mobileQuery = xmlParser.parse(stream);
-                    if (mobileQuery.getRetmsg().equals("OK")) {
-                        CLog.v(TAG, mobileQuery.getCity());
-                        tv_result.setText(mobileQuery.getCity() + mobileQuery.getSupplier());
-                    } else
-                        tv_result.setText("查无此号");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    tv_result.setText("查无此号");
-                }
-            } else if (msg.what == Constants.FAILED_TO_SEND) {
-                Bundle bundle = msg.getData();
-                String message = bundle.getString("MSG");
-                CLog.e(TAG, message);
-                tv_result.setText("查无此号");
-
-            }
-        }
-    };
->>>>>>> 9b01e5eaaa4cc8d3b8ecaf29118bb3c28ced29f9
 }
