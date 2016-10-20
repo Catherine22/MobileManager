aeaf54ef737f18a98f2d993434dc79f4b663d56e
Catherine <yacatherine19@gmail.com>
2016-10-20 14:10:08 +0800

    added widget

---
+++ app/src/main/java/com/itheima/mobilesafe/ui/MyAppWidgetProvider.java
@@ -0,0 +1,12 @@
+package com.itheima.mobilesafe.ui;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Catherine on 2016/10/20.
+ * Soft-World Inc.
+ * catherine919@soft-world.com.tw
+ */
+
+public class MyAppWidgetProvider extends AppWidgetProvider {
+}
